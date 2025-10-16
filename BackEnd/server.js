762008bb6446ed87e1f3a676980b82e64eea624a
shaikh shadab <112573477+shkshadab-shk@users.noreply.github.com>
2025-10-16 20:40:37 +0530

    Refactor server.js to use environment variables
    Modify server startup logic to use environment variable for port and conditionally start the server based on NODE_ENV.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,6 +1,14 @@
 require('dotenv').config()
 const app = require('./src/app')
 
-app.listen(3000, () => {
-   console.log('server is running on http://localhost:3000')
-})
+const PORT = process.env.PORT || 3000
+
+// For local development only
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+  })
+}
+
+// Export for Vercel
+module.exports = app
