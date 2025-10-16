fd8d70a574f77de63c11bb3aba194c98bad4dbd9
shaikh shadab <112573477+shkshadab-shk@users.noreply.github.com>
2025-10-16 20:15:27 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -3,7 +3,7 @@ const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
 const app = express()
 
-app.use(cors(https://ai-powered-code-reviewer-mu.vercel.app/))
+app.use(cors())
 
 app.use(express.json())
 
