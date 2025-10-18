25c9a9ce2596fd1a35c4c5175ae6731ff3919505
shkshadab-shk <shk.shadab.a@gmail.com>
2025-10-18 13:54:50 +0530

    server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -3,14 +3,6 @@ const app = require('./src/app')
 
 
 
-// For local development only
-// Use it in your fetch
-fetch(`${API_URL}/ai/get-review`, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-  body: JSON.stringify(yourData)
+app.listen(3000, () => {
+    console.log('Server is running on http://localhost:3000')
 })
-// Export for Vercel
-module.exports = app
--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,25 +1,16 @@
 const express = require('express');
 const aiRoutes = require('./routes/ai.routes')
 const cors = require('cors')
+
 const app = express()
 
-// Configure CORS for your frontend domain
-app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'http://localhost:5173',
-    'http://localhost:5174',
-    'https://aicodereview-one.vercel.app/', // Replace with your actual frontend URL
-    process.env.FRONTEND_URL // Add this to your Vercel env variables
-  ],
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
-}))
+app.use(cors())
+
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+    res.send('Hello World')
 })
 
 app.use('/ai', aiRoutes)
